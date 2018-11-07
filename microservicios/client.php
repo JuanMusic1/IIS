f6a33d6021352d39dfa8cbf5d2cdc1899b916c5c
JuanMusic1 <jsvelsaa@hotmail.com>
2018-11-07 08:56:17 -0500

    SOAP

--- microservicios/client.php
+++ microservicios/client.php
@@ -1,18 +1,17 @@
 <?php
 // modelo
-class Book
-{
+class Book{
         public $name;
         public $year;
 }
 
 // crear una instancia y seleccionar el libro a busccar
-$book      =new Book();
-$book->name='test 2';
+$book = new Book();
+$book -> name = 'test 2';
 
 // initialize SOAP client and call web service function
-$client=new SoapClient('https://nagios.bpoconsultores.com.co/eia/soap/server.php?wsdl',['trace'=>1,'cache_wsdl'=>WSDL_CACHE_NONE]);
-$resp=$client->bookYear($book);
+$client = new SoapClient('https://nagios.bpoconsultores.com.co/eia/soap/server.php?wsdl',['trace' => 1,'cache_wsdl' => WSDL_CACHE_NONE]);
+$resp = $client->bookYear($book);
 
 // dump respuesta
 var_dump($resp);
--- microservicios/server.php
+++ microservicios/server.php
@@ -12,13 +12,13 @@ class Book{
 function bookYear($book){
         // lista de libros
         $_books=[
-                ['name'=>'test 1','year'=>2011],
-                ['name'=>'test 2','year'=>2012],
-                ['name'=>'test 3','year'=>2013],
+                ['name' => 'test 1','year' => 2011],
+                ['name' => 'test 2','year' => 2012],
+                ['name' => 'test 3','year' => 2013],
         ];
         // busqueda por nombre
         foreach($_books as $bk)
-                if($bk['name']==$book->name)
+                if($bk['name'] == $book->name)
                         return $bk['year']; // libro encontrado
 
         return 0; // libro no encontrado
@@ -32,9 +32,9 @@ $server = new SoapServer("test.wsdl",[
 ]);
 
 // registro de las funciones disponibles
-$server->addFunction('bookYear');
+$server -> addFunction('bookYear');
 
 // inicio de la recepcion de mensajes
-$server->handle();
+$server -> handle();
 
 ?>
